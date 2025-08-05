af9e2e9add72ab689a5d08ee5bc10b352233b0e8
Jonathan Molina Fuentes <jonathan.molina.fuentes@bbva.com>
2025-08-05 18:33:40 -0500

    cron and getById, getAllCylinders

--- src/server.js
+++ src/server.js
@@ -1,17 +1,47 @@
 import express from 'express';
 import { ENV } from './config/env.js';
 import { db } from './config/db.js'
-import { cylinderTable, cylinderLogTable } from './db/schema.js';
+import { cylinderTable } from './db/schema.js';
 import { eq } from 'drizzle-orm';
+import job from './config/cron.js';
 
 const app = express();
-const PORT = ENV.PORT || 3001;
+const PORT = ENV.PORT || 5001;
+
+if (ENV.NODE_ENV === 'production') job.start();
 
 app.use(express.json());
 
 app.get('/api/test', (req, res) => {
   res.status(200).json({ success: true })
 })
+
+app.get('/api/cylinder/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const getCylinderById = await db
+      .select()
+      .from(cylinderTable)
+      .where(eq(cylinderTable.id, id));
+
+    res.status(200).json(getCylinderById);
+  } catch (e) {
+    console.log('Error en tx', e)
+  }
+});
+
+app.get('/api/cylinders', async (req, res) => {
+  try {
+    const cylinderList = await db
+      .select()
+      .from(cylinderTable)
+      .orderBy(cylinderTable.id, cylinderTable.id);
+
+    res.status(200).json([...cylinderList]);
+  } catch (e) {
+    console.log('Error en tx', e)
+  }
+});
 
 app.post('/api/cylinder', async (req, res) => {
   try {
